b74e4b009f0e4d11a231328325fbcc7be352de91
chapu <molinero.cristian.d@gmail.com>
2019-09-21 14:41:49 -0300

    margin corrections

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function () {
   return (
     <div>
-      <h1>Busca el articulo que quieras</h1>
+      <h1 className="h1__welcome">Busca el articulo que quieras</h1>
     </div>
   );
 }
